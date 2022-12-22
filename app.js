23b210c5aa9bde67056d4e3c3bc605c0bf9dfe53
Ayush-0801 <ayush19301@iiitd.ac.in>
2022-12-22 21:18:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
  var bodyParser = require("body-parser");
  var http=require('http');
  var WebSocket = require('ws');
+ const fs = require('fs');
 
 const port = process.env.PORT || '5000';
  
@@ -354,7 +355,21 @@ server.listen(port, () => {
  
  
  function queueUpdateBroadcast(queue,song,seek)
- {
+ {      
+    // parse json
+    var jsonObj = JSON.parse(queue);
+      
+    // stringify JSON Object
+    var jsonContent = JSON.stringify(jsonObj);
+      
+    fs.writeFile("backup.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+      
+        console.log("JSON file has been saved.");
+    });
     wss.clients.forEach((ws) => {
           ws.send(
             JSON.stringify(
