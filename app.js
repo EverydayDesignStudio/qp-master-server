c240dc03a989357900c9e53e63640ca182b35045
Ayush-0801 <ayush19301@iiitd.ac.in>
2023-04-05 21:32:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -274,9 +274,7 @@ function processDatabase(qpData,user)
 
 function queueUpdateUser(queue, additionToQueue, offset, user)
 {
-  console.log("offset: ", offset);
   queue.splice(offset,queue.length-offset);
-  console.log(queue);
   queue=queue.concat(additionToQueue);
 
   while(queue.length<4)
